d89d60ae2acae48d597dcbd631edcf3c2f30650e
georgembugua <georgembugua2@student.moringaschool.com>
2025-03-27 13:14:45 +0300

    image upload const

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const gallery = document.getElementById("gallery");
     const searchinput = document.getElementById("searchInput");
-    
-})
+    const uploadButton = document.getElementById("uploadButton");
+    const imageUpload = document.getElementById("imageUpload"
