7a37875c864e710db9853d81659df3a2de185d30
georgembugua <georgembugua2@student.moringaschool.com>
2025-03-27 13:15:56 +0300

    added image title

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const gallery = document.getElementById("gallery");
     const searchinput = document.getElementById("searchInput");
     const uploadButton = document.getElementById("uploadButton");
-    const imageUpload = document.getElementById("imageUpload"
+    const imageUpload = document.getElementById("imageUpload");
+    const imageTitle = document.getElementById("imageTitle");
